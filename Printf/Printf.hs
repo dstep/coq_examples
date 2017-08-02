8b2b39a609366f2ca0fec0e219c1d3525e18ae32
Denis <stepanov.denis91@gmail.com>
2017-08-02 22:21:12 +0300

    Simple printf example

---
+++ Printf/Printf.hs
@@ -0,0 +1,194 @@
+{-# OPTIONS_GHC -cpp -XMagicHash #-}
+{- For Hugs, use the option -F"cpp -P -traditional" -}
+
+module Printf where
+
+import qualified Prelude
+
+#ifdef __GLASGOW_HASKELL__
+import qualified GHC.Base
+import qualified GHC.Prim
+import qualified Data.Bits
+import qualified Data.Char
+#else
+-- HUGS
+import qualified IOExts
+#endif
+
+#ifdef __GLASGOW_HASKELL__
+unsafeCoerce :: a -> b
+unsafeCoerce = GHC.Base.unsafeCoerce#
+#else
+-- HUGS
+unsafeCoerce :: a -> b
+unsafeCoerce = IOExts.unsafeCoerce
+#endif
+
+#ifdef __GLASGOW_HASKELL__
+type Any = GHC.Prim.Any
+#else
+-- HUGS
+type Any = ()
+#endif
+
+type Sig a =
+  a
+  -- singleton inductive, whose constructor was exist
+  
+proj1_sig :: a1 -> a1
+proj1_sig e =
+  e
+
+append :: Prelude.String -> Prelude.String ->
+          Prelude.String
+append s1 s2 =
+  case s1 of {
+   ([]) -> s2;
+   (:) c s1' -> (:) c (append s1' s2)}
+
+type TypeForFormatString = Any
+
+last_digit :: Prelude.Integer -> Prelude.Char
+last_digit n =
+  (\fO fS n -> if n Prelude.== 0 then fO () else fS (n Prelude.- 1))
+    (\_ ->
+    '0')
+    (\n0 ->
+    (\fO fS n -> if n Prelude.== 0 then fO () else fS (n Prelude.- 1))
+      (\_ ->
+      '1')
+      (\n' ->
+      last_digit n')
+      n0)
+    n
+
+div2 :: Prelude.Integer -> Prelude.Integer
+div2 n =
+  (\fO fS n -> if n Prelude.== 0 then fO () else fS (n Prelude.- 1))
+    (\_ ->
+    0)
+    (\n0 ->
+    (\fO fS n -> if n Prelude.== 0 then fO () else fS (n Prelude.- 1))
+      (\_ ->
+      0)
+      (\n' -> Prelude.succ
+      (div2 n'))
+      n0)
+    n
+
+nat_to_str :: Prelude.Integer ->
+              Prelude.String
+nat_to_str x =
+  let {
+   nat_to_str0 = \n ->
+    nat_to_str (proj1_sig n)}
+  in
+  let {filtered_var = div2 x} in
+  (\fO fS n -> if n Prelude.== 0 then fO () else fS (n Prelude.- 1))
+    (\_ -> (:) (last_digit x)
+    ([]))
+    (\n' ->
+    append (nat_to_str0 n') ((:)
+      (last_digit x) ([])))
+    filtered_var
+
+printf_ :: Prelude.String -> Prelude.String ->
+           TypeForFormatString
+printf_ s pref =
+  case s of {
+   ([]) -> unsafeCoerce pref;
+   (:) x xs ->
+    (\f a -> f (Data.Bits.testBit (Data.Char.ord a) 0)
+              (Data.Bits.testBit (Data.Char.ord a) 1)
+              (Data.Bits.testBit (Data.Char.ord a) 2)
+              (Data.Bits.testBit (Data.Char.ord a) 3)
+              (Data.Bits.testBit (Data.Char.ord a) 4)
+              (Data.Bits.testBit (Data.Char.ord a) 5)
+              (Data.Bits.testBit (Data.Char.ord a) 6)
+              (Data.Bits.testBit (Data.Char.ord a) 7))
+      (\b b0 b1 b2 b3 b4 b5 b6 ->
+      case b of {
+       Prelude.True ->
+        case b0 of {
+         Prelude.True ->
+          case b1 of {
+           Prelude.True ->
+            printf_ xs
+              (append pref ((:) x ([])));
+           Prelude.False ->
+            case b2 of {
+             Prelude.True ->
+              printf_ xs
+                (append pref ((:) x ([])));
+             Prelude.False ->
+              case b3 of {
+               Prelude.True ->
+                printf_ xs
+                  (append pref ((:) x ([])));
+               Prelude.False ->
+                case b4 of {
+                 Prelude.True ->
+                  case b5 of {
+                   Prelude.True ->
+                    printf_ xs
+                      (append pref ((:) x
+                        ([])));
+                   Prelude.False ->
+                    case b6 of {
+                     Prelude.True ->
+                      printf_ xs
+                        (append pref ((:) x
+                          ([])));
+                     Prelude.False ->
+                      unsafeCoerce (\x0 ->
+                        printf_ xs
+                          (append pref
+                            (nat_to_str x0)))}};
+                 Prelude.False ->
+                  printf_ xs
+                    (append pref ((:) x ([])))}}}};
+         Prelude.False ->
+          case b1 of {
+           Prelude.True ->
+            case b2 of {
+             Prelude.True ->
+              printf_ xs
+                (append pref ((:) x ([])));
+             Prelude.False ->
+              case b3 of {
+               Prelude.True ->
+                printf_ xs
+                  (append pref ((:) x ([])));
+               Prelude.False ->
+                case b4 of {
+                 Prelude.True ->
+                  case b5 of {
+                   Prelude.True ->
+                    printf_ xs
+                      (append pref ((:) x
+                        ([])));
+                   Prelude.False ->
+                    case b6 of {
+                     Prelude.True ->
+                      printf_ xs
+                        (append pref ((:) x
+                          ([])));
+                     Prelude.False ->
+                      unsafeCoerce (\s0 ->
+                        printf_ xs
+                          (append pref s0))}};
+                 Prelude.False ->
+                  printf_ xs
+                    (append pref ((:) x ([])))}}};
+           Prelude.False ->
+            printf_ xs
+              (append pref ((:) x ([])))}};
+       Prelude.False ->
+        printf_ xs (append pref ((:) x ([])))})
+      x}
+
+printf :: Prelude.String ->
+          TypeForFormatString
+printf s =
+  printf_ s ([])
+
---
+++ Printf/Printf.v
@@ -0,0 +1,128 @@
+(* Standard library for strings *)
+Require Import String.
+Require Import Ascii.
+
+(* So "xyz" notation is a string now *)
+Open Scope string.
+
+(* Gets a format string and returns a type of the printf function with that format string *)
+Fixpoint TypeForFormatString (s:string) : Type :=
+match s with
+| EmptyString => string
+| String "#" xs => nat-> TypeForFormatString xs
+| String "%" xs => string->TypeForFormatString xs
+| String _ xs => TypeForFormatString xs
+end.
+
+(* Some assertions *)
+(* "cbv; auto" means it is checked by a direct evaluation *)
+Goal (TypeForFormatString "no format specifiers" = string). 
+cbv; auto.
+Qed.
+
+Goal (TypeForFormatString "a single string expected: %" = (string->string)). 
+cbv; auto.
+Qed.
+
+Goal (TypeForFormatString "String and two numbers: % (#, #)" = (string->nat->nat->string)). 
+cbv; auto.
+Qed.
+
+(* The following is about converting number to a binary string representation *)
+Section NatToString.
+
+  (* Extracts the last digit of a number as an ascii character *)
+  Fixpoint last_digit (n:nat) : ascii :=
+  match n with
+  | O => "0"
+  | S O => "1"
+  | S (S n') => last_digit n'
+  end.
+
+  (* Special stuff for non-structural recursion *)
+  Require Import Program.Wf.
+
+  (* Division by 2 *)
+  Fixpoint div2 (n:nat) : nat :=
+  match n with
+  | O => O
+  | S O => O
+  | S (S n') => S (div2 n')
+  end.
+
+  (* Useful theorems for proving the next properties *)
+  Require Import Arith.
+  Require Import Omega.
+
+  (* Every non-zero number divided by two is less than the original number *)
+  Theorem div_decreases : forall n:nat, n <> O -> div2 n < n.
+  induction n; firstorder.
+  destruct n; firstorder.
+  simpl.
+  apply Lt.lt_n_S.
+  cut (div2 n <= div2 (S n)).
+  omega.
+
+    Lemma div2_nonincr_helper : forall n:nat, div2 n <= div2 (S n) /\ div2 (S n) <= div2 (S (S n)).
+    induction n.
+    firstorder.
+    destruct IHn.
+    constructor.
+    auto.
+    simpl div2 at 1.
+    remember (S n) as Sn.
+    simpl.
+    omega.
+    Qed.
+
+    Lemma div2_nonincr : forall n:nat, div2 n <= div2 (S n).
+    apply div2_nonincr_helper.
+    Qed.
+
+  apply div2_nonincr.
+  Qed.
+
+  (* Finally, the program for converting number to a string *)
+  (* measure answers the question "why this recursive function terminating?"
+     {measure n} means "because n decreases"  *)
+  Program Fixpoint nat_to_str (n:nat) {measure n} : string :=
+  match div2 n as n1 with
+  | O => String (last_digit n) EmptyString
+  | S n' => (nat_to_str n' (*the proof that n' < n is Obligation 1*)) ++ String (last_digit n) EmptyString
+  end.
+
+  (* Proving that n is atually decreases with every recursive call *)
+  Obligation 1.
+  apply Lt.lt_S_n.
+  rewrite Heq_n1.
+  transitivity n.
+  apply div_decreases.
+  destruct n.
+  inversion Heq_n1.
+  auto.
+  auto.
+  (* Obligation is solved, so program can now be accepted by Coq *)
+  Defined.
+End NatToString.
+
+(* The core printf function. Notice how its return type depends on the parameter s *)
+Fixpoint Printf_ (s:string) (pref:string) : TypeForFormatString(s) :=
+match s as s' return TypeForFormatString(s') with
+  (*With a dependent type, you need to state upfront what type match must return*)
+| EmptyString => pref
+| String "#" xs => fun x => Printf_ xs (pref ++ nat_to_str x)
+| String "%" xs => fun s => Printf_ xs (pref ++ s)
+| String x xs => Printf_ xs (pref ++ String x EmptyString)
+end.
+
+(* Wrapper for printf *)
+Definition Printf (s:string) := Printf_ s ""%string.
+
+(* A few tests *)
+Eval compute in (Printf "My format string: % % %" "test" "q" "w")%string.
+(* = "My format string: test q w" *)
+Eval compute in (Printf "Current year is # and I'm # years old" (2017) (2017-1991))%string.
+(*= "Current year is 1111100011 and I'm 1100 years old"*)
+
+Extraction Language Haskell.
+Extraction "Printf.hs" Printf.
